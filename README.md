5a49b2197ca622b63abef540d41a4bba5851b406
Timothy Scheuering <interactivetimmy@gmail.com>
2022-12-31 18:44:27 -0500

    feat: add scafolding for project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Portfolio
-My Portfolio - Draft 1
+
+Portfolio for Tai Roetzer
---
+++ static/scripts/scroll-handler.js
@@ -0,0 +1,20 @@
+class ScrollHandler {
+  constructor() {
+    this.deltaY = 0;
+    this.positionY = 0;
+  }
+
+  update({ positionY }) {
+    if (this.positionY < 0)
+  }
+}
+
+window.addEventListener('scroll', () => {
+  const CONSTANTS = {
+    
+  };
+
+  const { scrollY } = window;
+
+  if (y >= )
+})
